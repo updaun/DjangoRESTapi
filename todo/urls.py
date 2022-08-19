4886c1509e4710a06d3750d99c2f369381d56ea1
updaun <updauney@daum.net>
2022-08-19 16:55:09 +0900

    todo example

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+from .views import TodoAPIView, TodosAPIView, DoneTodoAPIView, DoneTodosAPIView
+
+urlpatterns = [
+    path('todo/', TodosAPIView.as_view()),
+    path('todo/<int:pk>/', TodoAPIView.as_view()),
+    path('done/', DoneTodosAPIView.as_view()),
+    path('done/<int:pk>/', DoneTodoAPIView.as_view()),
+]
